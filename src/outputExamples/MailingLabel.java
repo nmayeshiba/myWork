1e7842165cec2c39ec6e08e4b7ef552a210c5dd0
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-17 09:14:16 -0500

    Mailing Label Program

---
+++ src/outputExamples/MailingLabel.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class MailingLabel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Noah Mayeshiba \n1620 Greenview Drive \nBrookfield, WI 53045");
+	}
+
+}
