c31abec03768505d19d58205adb7c8a25e733294
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-18 08:47:59 -0500

    Favorite Movie Program Edit

--- src/outputExamples/FavoriteMovie.java
+++ src/outputExamples/FavoriteMovie.java
@@ -1,10 +1,15 @@
 package outputExamples;
-
-public class FavoriteMovie {
+import java.util.Scanner;
+public class FavoriteMovie 
+{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Favorite Movie: Pacific Rim");
+		Scanner input = new Scanner(System.in);
+		String movie;
+		System.out.println("Tell me your favorite movie.");
+		movie = input.nextLine();
+		System.out.println(movie + " is your favorite movie? Lame.");
 	}
 
-}
+}	
