bc338e8c06231d9265eed5d6c850b436df24cf95
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-20 08:31:19 -0500

    Variable Practice Program 2

---
+++ src/outputExamples/VariablesPractice2.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class VariablesPractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double mysterynumber = 13.33;
+		int dead = 1234567;
+		final char middleinitial = 'z';
+		System.out.println("The mystery number is " +mysterynumber + ".");
+		System.out.println("The middle initial is " + middleinitial + ".");
+	}
+
+}
