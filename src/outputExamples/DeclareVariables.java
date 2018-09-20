bfb63cce738f486f2e670541d0d14543babebc09
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-20 08:27:55 -0500

    Variable Practice Program 1

--- src/outputExamples/DeclareVariables.java
+++ src/outputExamples/DeclareVariables.java
@@ -4,7 +4,14 @@ public class DeclareVariables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		final int age = 17;
+		final char letter = 'm';
+		final double classes = 5.0;
+		final boolean question = true;
+		System.out.println("Your age is " + age);
+		System.out.println("The letter is " + letter);
+		System.out.println("You are taking " + classes + " classes this term.");
+		System.out.println("The question is " + question);
 	}
 
 }
