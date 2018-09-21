51346fc9590d43a2edfbf1e03d487c6dcc801d1f
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-21 09:01:10 -0500

    Monkey Program Change

--- src/inputExamples/Monkey.java
+++ src/inputExamples/Monkey.java
@@ -5,7 +5,7 @@ public class Monkey {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.println("What is the height of an adult  M O N K E Y");
+		System.out.println("What is the height of an adult  M O N K E Y ?");
 		int height = input.nextInt();
 		System.out.println("So you're telling me a M O N K E Y is " +height +" inches tall?");
 		boolean answer = input.nextBoolean();
@@ -14,7 +14,9 @@ public class Monkey {
 		System.out.println("You are a strange man");	
 		} else 
 			{
-			System.out.println("oh.");
+			System.out.println("Then what is the height of a MONKEY?");
+			height = input.nextInt();
+			System.out.println("Okay then, I guess a monkey is " + height + "feet tall...");
 			}
 	}
 
