efec1499f5a98da44fd6ca9053d99f88ac9a50d6
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-25 08:19:50 -0500

    Vote Program

---
+++ src/inputExamples/Vote.java
@@ -0,0 +1,13 @@
+package inputExamples;
+import java.util.Scanner;
+public class Vote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Did you vote??????");
+		boolean vote = input.nextBoolean();
+		System.out.println("Wow, your voting value was " +vote +"? Congratulations.");
+	}
+
+}
