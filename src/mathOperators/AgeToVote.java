fa9bf9a0c471f97a97430684e7301063551e820d
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-09-26 09:03:17 -0500

    Age to Vote MathOps Program

--- src/mathOperators/AgeToVote.java
+++ src/mathOperators/AgeToVote.java
@@ -1,6 +1,8 @@
 package mathOperators;
+
 import java.util.Scanner;
-public class Age {
+
+public class AgeToVote {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,8 +10,14 @@ public class Age {
 		System.out.println("WHAT is your age?");
 		int age = input.nextInt();
 		System.out.println("In 17,386 years, you'll be dead.");
-		age = age + 7;
-		System.out.println("However, in 7 years, you'll be " +age + " years old.");
+		age = 18 - age;
+		if(age == 1) 
+		{
+		System.out.println("However, in " + age + " year, you'll be able to vote.");	
+		} else 
+			{
+			System.out.println("However, in " + age + " years, you'll be able to vote.");
+			}
 		input.close();
 	}
 
