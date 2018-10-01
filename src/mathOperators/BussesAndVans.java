4a7bb5a30c4fe81410231a48a721fad892963020
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-01 07:34:45 -0500

    Busses and Vans but I Kinda Cheated

--- src/mathOperators/BussesAndVans.java
+++ src/mathOperators/BussesAndVans.java
@@ -11,12 +11,15 @@ public class BussesAndVans {
 		int vans = 0;
 		System.out.println("How many people need transportation?");
 		numberOfPeople = input.nextInt();
-		if (numberOfPeople < busCapacity)
+		if (numberOfPeople <= 34)
 		{
 		vans = numberOfPeople;
-		System.out.println("You don't have enough people for busses, so you need " + vans + " vans.");
+		System.out.println("You don't have enough people for busses, so you need " + vans + " people to go in vans.");
 		}
-		vans = numberOfPeople / busCapacity;
+		else
+		busses = numberOfPeople / busCapacity;
+		vans = numberOfPeople - (busCapacity*busses);
+		System.out.println("You need " + busses + " busses and have " + vans + " people to transport in vans. ");
 		// This program is unfinished, don't forget to finish it.
 		input.close();
 	}
