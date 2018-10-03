185feed77bfd2d90b05e7d8bc763dff3584eb9fb
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-03 08:57:06 -0500

    Programming Opportunities 0.0.2

--- src/mathOperators/LetsPaint.java
+++ src/mathOperators/LetsPaint.java
@@ -5,7 +5,8 @@ public class LetsPaint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		
+		System.out.println("This program is on my other computer.");
+		input.close();
 	}
 
 }
--- src/mathOperators/MakeSomeChange.java
+++ src/mathOperators/MakeSomeChange.java
@@ -5,15 +5,9 @@ public class MakeSomeChange {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		double amount = 0;
-		System.out.println("How much money am I making change for?");
-		amount = input.nextDouble();
-	    (int)dollars = amount / 1;
-	    amount = amount - dollars;
-	    (int)quarters = amount / 0.25;
-	    amount = amount - quarters;
-	    (int)dimes = amount / 0.1;
-	    //I'll deal with this program later.
+		System.out.println("This program is on my other computer.");
+		input.close();
+	    //I'll deal with this program later. DON'T FORGET MODULAR DIVISON (%)
 	    
 	}
 
