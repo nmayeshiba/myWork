9ed330466d337e3c858ee946bc24a0fe7ff964ea
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-05 09:02:59 -0500

    Real Greeter Class (I accidentally put it in dev)

---
+++ src/objectOrientedProgramming/Greeter.java
@@ -0,0 +1,15 @@
+package objectOrientedProgramming;
+
+public class Greeter {
+
+//instance fields - data storage
+	
+//constructors - methods for constructing the object
+	
+//default constructor
+public Greeter() 
+	{
+	//Statements to initialize the state of the object
+	}
+//methods - things the object can do.
+}
