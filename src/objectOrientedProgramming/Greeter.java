90678de006a3c66fc9f9bc8425d7a64c570d2bdc
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-10 08:07:39 -0500

    Greeter Class 0.0.1

--- src/objectOrientedProgramming/Greeter.java
+++ src/objectOrientedProgramming/Greeter.java
@@ -11,5 +11,9 @@ public Greeter()
 	{
 	//Statements to initialize the state of the object
 	}
+public void sayHello() 
+{
+	System.out.println("Hello?");
+}
 //methods - things the object can do.
 }
