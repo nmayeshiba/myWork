e8766c7a3c7551580c058d8fdb6754c53f1b0db5
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-10 08:07:15 -0500

    Greeter Test Class

---
+++ src/objectOrientedProgramming/TestClass.java
@@ -0,0 +1,16 @@
+package objectOrientedProgramming;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter bob = new Greeter();
+		Friend fred = new Friend();
+		bob.sayHello();
+		bob.sayHello();
+		bob.sayHello();
+		fred.sayMessage();
+		bob.sayHello();
+	}
+
+}
