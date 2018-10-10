f3a8f4a5e9184a6c71d94e09bdbbda313a7da04b
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-10 08:07:48 -0500

    Friend Test Class

---
+++ src/objectOrientedProgramming/TestClass2.java
@@ -0,0 +1,17 @@
+package objectOrientedProgramming;
+
+public class TestClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("unused")
+		Friend jenny = new Friend();
+		Friend billy = new Friend();
+		@SuppressWarnings("unused")
+		Friend sammy = new Friend();
+		Friend kyle = new Friend();
+		billy.sayMessage();
+		kyle.sayMessage();
+	}
+
+}
