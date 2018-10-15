a9322a07d347dcd1fbc5b0c84beffe8d49c60d84
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-15 08:02:13 -0500

    Numbers Test Class

---
+++ src/objectOrientedProgramming/TestClass4.java
@@ -0,0 +1,13 @@
+package objectOrientedProgramming;
+
+public class TestClass4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Numbers man = new Numbers();
+		man.sayNumber(3);
+		man.sayNumberPlus2(9);
+		man.saySum(1042, 3);
+	}
+
+}
