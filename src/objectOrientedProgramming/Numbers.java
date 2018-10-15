80c730a5cc64b0878ff34a7d79d16b93f2fc3e35
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-10-15 08:02:24 -0500

    Numbers Class

---
+++ src/objectOrientedProgramming/Numbers.java
@@ -0,0 +1,23 @@
+package objectOrientedProgramming;
+
+public class Numbers {
+
+public Numbers()
+	{
+	
+	}
+public void sayNumber(double a)
+	{
+	System.out.println("The number you typed is " + a + ", right?");
+	}
+public void sayNumberPlus2(double b)
+	{
+	double addition = b + 2;
+	System.out.println("When you add two to your number, you get " + addition +".");
+	}
+public void saySum(double a, double b)
+	{
+	double whatever = a + b;
+	System.out.println("When you add your numbers together, you get "+ whatever + ".");
+	}
+}
