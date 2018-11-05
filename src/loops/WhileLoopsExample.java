121bae655452a63a50554fa6bef01ca69f3c85c5
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-11-05 08:34:04 -0600

    1 to 10 Class

---
+++ src/loops/WhileLoopsExample.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class WhileLoopsExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 0;
+		while(num <= 100)
+		{
+		System.out.println(num);
+		num++;
+		}
+		System.out.println("The program has concluded.");
+	}
+
+}
