a94425c3780e0cf243a177095077751d172dab98
Noah Mayeshiba <n.mayeshiba@gmail.com>
2018-11-05 08:40:03 -0600

    While Loop Numbers Class

--- src/loops/WhileLoopsExample.java
+++ src/loops/WhileLoopsExample.java
@@ -1,16 +1,20 @@
 package loops;
-
+import java.util.Scanner;
 public class WhileLoopsExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("How long should the loop go on for?");
+		int lengthOfLoop = input.nextInt();
 		int num = 0;
-		while(num <= 100)
+		while(num <= lengthOfLoop)
 		{
 		System.out.println(num);
-		num++;
+		num = num + 2;
 		}
 		System.out.println("The program has concluded.");
+		input.close();
 	}
 
 }
