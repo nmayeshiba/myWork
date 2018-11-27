d0ff1ea3c49728a31463ba124bc719fcfd4a2f6a
nmayeshiba <n.mayeshiba@gmail.com>
2018-11-27 08:09:19 -0600

    Random Numbers Programs

---
+++ src/randomNumbers/RollDieV2.java
@@ -0,0 +1,55 @@
+package randomNumbers;
+import java.util.Random;
+import java.util.Scanner;
+public class RollDieV2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random generator = new Random();
+		Scanner input = new Scanner(System.in);
+		int sides = 6;
+		int oneRolls = 0;
+		int onePercent = oneRolls / sides;
+		int twoRolls = 0;
+		int threeRolls = 0;
+		int fourRolls = 0;
+		int fiveRolls = 0;
+		int sixRolls = 0;
+		sides = sides + 1;
+		System.out.println("How many times should the die be rolled?");
+		int rolls = input.nextInt();
+		int die = 0;
+		for(int counter = 1; counter <= rolls; counter++)
+		{
+		die = generator.nextInt(sides);	
+		System.out.println("You rolled: " + die);
+		if(die == 1)
+			{
+			oneRolls++;
+			}
+		if(die == 2)
+			{
+			twoRolls++;	
+			}
+		if(die == 3)
+			{
+			threeRolls++;
+			}
+		if(die == 4)
+			{
+			fourRolls++;
+			}
+		if(die == 5)
+			{
+			fiveRolls++;
+			}
+		if(die == 6)
+			{
+			sixRolls++;
+			}
+		}
+		System.out.println("One was rolled " + oneRolls + " times " + onePercent + "%.");
+		input.close();
+	}
+
+}
